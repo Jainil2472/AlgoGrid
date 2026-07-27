dd656bdaecbe00c57cfb9a596f4656afa75fd611
Jainil2472 <pjainil002@gmail.com>
2026-07-28 00:08:23 +0530

    first commit

---
+++ Frontend/frontend/src/component/Button.jsx
@@ -0,0 +1,23 @@
+const variantClasses = {
+    primary: ' bg-primary text-white border-2 border-primary shadow-sm rounded-md hover:bg-white hover:text-black ',
+}
+
+const sizeClasses = {
+    lg:'w-full h-11 text-lg'
+}
+
+function Button({
+    children,
+    size = 'lg',    
+    variant ='primary',
+    type = 'button',
+    className = ''
+}){
+
+    return(
+    <button type={type} className={`${variantClasses[variant]} ${sizeClasses[size]} ${className}`}>
+        {children}
+    </button>
+    );
+}
+export default Button;
---
+++ Frontend/frontend/src/component/NavBar.jsx
@@ -0,0 +1,9 @@
+
+
+
+function NavBar(){
+    return<>
+    hello
+    </>
+}
+export default NavBar;
